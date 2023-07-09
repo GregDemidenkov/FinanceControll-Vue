fc61b7e0da0238eadf4a4fa8c927c5be7500020e
podlunoy <greg.demidenkov@yandex.ru>
2023-07-09 19:57:17 +0300

    Refactor Budget model

--- server/infrastructure/db/models/Budget.js
+++ server/infrastructure/db/models/Budget.js
@@ -2,11 +2,11 @@ const { Schema, model, ObjectId } = require("mongoose")
 
 
 const Budget = new Schema({
-    from_date: {type: Date, required: true},
+    from_date: {type: Date, default: Date.now()},
     to_date: {type: Date, default: null},
     status: {type: String, default: "active"},
     budget_money: {type: Number, required: true},
-    curr_money: {type: Number, required: true},
+    curr_money: {type: Number, default: 0},
     account_id: {type: ObjectId, required: true}
 })
 
